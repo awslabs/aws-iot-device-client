ef0bf71ac6f7d504484da336b5a3da510b88ba5e
Francis Fu <fufranci@amazon.com>
2020-12-03 17:01:44 -0800

    Add more tests

--- test/config/TestConfig.cpp
+++ test/config/TestConfig.cpp
@@ -133,3 +133,60 @@ TEST(Config, SecureTunnelingCli)
     ASSERT_EQ(22, config.tunneling.port.value());
     ASSERT_FALSE(config.tunneling.subscribeNotification);
 }
+
+TEST(Config, SecureTunnelingDisableSubscription)
+{
+    constexpr char jsonString[] = R"(
+{
+	"endpoint": "endpoint value",
+	"cert": "cert value",
+	"key": "key value",
+	"root-ca": "root-ca value",
+	"thing-name": "thing-name value",
+    "tunneling": {
+        "enabled": true,
+        "subscribe-notification": false,
+        "destination-access-token": "destination access token value",
+        "region": "region value",
+        "port": 65535
+    }
+})";
+    JsonObject jsonObject(jsonString);
+    JsonView jsonView = jsonObject.View();
+
+    PlainConfig config;
+    config.LoadFromJson(jsonView);
+
+    ASSERT_TRUE(config.Validate());
+    ASSERT_TRUE(config.tunneling.enabled);
+    ASSERT_FALSE(config.tunneling.subscribeNotification);
+    ASSERT_STREQ("destination access token value", config.tunneling.destinationAccessToken->c_str());
+    ASSERT_STREQ("region value", config.tunneling.region->c_str());
+    ASSERT_EQ(65535, config.tunneling.port.value());
+}
+
+TEST(Config, SecureTunnelingInvalidPort)
+{
+    constexpr char jsonString[] = R"(
+{
+	"endpoint": "endpoint value",
+	"cert": "cert value",
+	"key": "key value",
+	"root-ca": "root-ca value",
+	"thing-name": "thing-name value",
+    "tunneling": {
+        "enabled": true,
+        "subscribe-notification": false,
+        "destination-access-token": "destination access token value",
+        "region": "region value",
+        "port": 65536
+    }
+})";
+    JsonObject jsonObject(jsonString);
+    JsonView jsonView = jsonObject.View();
+
+    PlainConfig config;
+    config.LoadFromJson(jsonView);
+
+    ASSERT_FALSE(config.Validate());
+}
