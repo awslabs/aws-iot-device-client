d86bc09ba9d4d205bbeba04fc2bb151f17ef4eab
Zach Giordano <giordz@amazon.com>
2020-12-14 22:55:43 -0800

    Adding compile mode and cmake args var to build script.

--- .github/build.sh
+++ .github/build.sh
@@ -1,44 +1,60 @@
 #!/bin/sh
-# Compiler name, g++ or clang, just need the first character
-compilerName=$(echo $1 | cut -c12-12)
-# Everything that follows the first character
-compilerExec=$(echo $1 | cut -c12-20)
-echo $compilerName
-echo $compilerExec
-if [ $# -eq 1 ]
-  then
-    apt-get update
-    apt-get install --assume-yes software-properties-common
-    # Install correct compiler from CI matrix from (APT)
-    if [ $compilerName = "g" ]
-      then
-        # Cuts to just the version ie, g++-{7}
-        ver=$(echo $compilerExec | cut -c5-20)
-        echo "ver"
-        echo $ver
-        add-apt-repository ppa:ubuntu-toolchain-r/test -y
-        apt-get update
-        apt-get --assume-yes install g++-$ver
 
-        export CXX=/usr/bin/g++-$ver
-    fi
-    if [ $compilerName = "c" ]
-      then
-        # Cuts to just the version ie, clang-{7}
-        ver=$(echo $compilerExec | cut -c7-20)
-        if [ $ver = "5" ] || [ $ver = "6" ]
-          then
-            ver=$(echo "$ver.0")
-        fi
-        echo "ver"
-        echo $ver
-        apt-get --assume-yes install apt-transport-https
-        wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
-        apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-$ver main" -y
-        apt-get update
-        apt-get --assume-yes install clang-$ver
-        export CXX=/usr/bin/clang++-$ver
-    fi
+cmakeArgs=""
+# Check if first argument is compile mode
+compileModeArgument=$(echo "$1" | cut -c3-14)
+if [ "$compileModeArgument" = "compile-mode" ]; then
+  compileMode=$(echo "$1" | cut -d "=" -f2-)
+  case $compileMode in
+    st_component_mode)
+    cmakeArgs="-DEXCLUDE_JOBS=ON -DEXCLUDE_DD=ON -DEXCLUDE_FP=ON -DDISABLE_MQTT=ON"
+    ;;
+    *)
+    echo "No compile mode match found"
+    ;;
+  esac
+else
+  # Compiler name, g++ or clang, just need the first character
+  compilerName=$(echo $1 | cut -c12-12)
+  # Everything that follows the first character
+  compilerExec=$(echo $1 | cut -c12-20)
+  echo $compilerName
+  echo $compilerExec
+  if [ $# -eq 1 ]
+    then
+      apt-get update
+      apt-get install --assume-yes software-properties-common
+      # Install correct compiler from CI matrix from (APT)
+      if [ $compilerName = "g" ]
+        then
+          # Cuts to just the version ie, g++-{7}
+          ver=$(echo $compilerExec | cut -c5-20)
+          echo "ver"
+          echo $ver
+          add-apt-repository ppa:ubuntu-toolchain-r/test -y
+          apt-get update
+          apt-get --assume-yes install g++-$ver
+
+          export CXX=/usr/bin/g++-$ver
+      fi
+      if [ $compilerName = "c" ]
+        then
+          # Cuts to just the version ie, clang-{7}
+          ver=$(echo $compilerExec | cut -c7-20)
+          if [ $ver = "5" ] || [ $ver = "6" ]
+            then
+              ver=$(echo "$ver.0")
+          fi
+          echo "ver"
+          echo $ver
+          apt-get --assume-yes install apt-transport-https
+          wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
+          apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-$ver main" -y
+          apt-get update
+          apt-get --assume-yes install clang-$ver
+          export CXX=/usr/bin/clang++-$ver
+      fi
+  fi
 fi
 
 ### Compile ###
@@ -49,7 +65,7 @@ if [ ! -d "./build" ]; then
 fi
 cd ./build/
 
-cmake ../ -DBUILD_SDK=OFF -DBUILD_TEST_DEPS=OFF
+cmake ../ -DBUILD_SDK=OFF -DBUILD_TEST_DEPS=OFF "$cmakeArgs"
 cmake --build . --target aws-iot-device-client
 if [ $? != 0 ]
 then
--- .github/docker-images/entry-script.sh
+++ .github/docker-images/entry-script.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 cd /root/aws-iot-device-client/.github/
-
-compileModeArgument=$(echo "$1" | cut -c3-14)
-if [ "$compileModeArgument" = "compile-mode" ]; then
-  compileMode=$(echo "$1" | cut -d "=" -f2-)
-  case $compileMode in
-    st_component_mode)
-    chmod +x ./build_st_component.sh
-    ./build_st_component.sh
-    ;;
-    *)
-    echo "No compile mode match found"
-    ;;
-  esac
-else
-  chmod +x ./build.sh
-  ./build.sh "$1"
-fi
+chmod +x ./build.sh
+./build.sh $1
