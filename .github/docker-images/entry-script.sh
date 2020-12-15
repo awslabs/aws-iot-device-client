9237f34a2a6dcf95c5f11a16fcfdd7f186e5c4e8
Zach Giordano <giordz@amazon.com>
2020-12-14 22:32:35 -0800

    Adding compile mode argument.

--- .github/docker-images/entry-script.sh
+++ .github/docker-images/entry-script.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 cd /root/aws-iot-device-client/.github/
-if [ -n "${ST_COMPONENT_MODE}" ]; then
-  chmod +x ./build_st_component.sh
-  ./build_st_component.sh
+
+compileModeArgument=$(echo "$1" | cut -c3-14)
+if [ "$compileModeArgument" = "compile-mode" ]; then
+  compileMode=$(echo "$1" | cut -d "=" -f2-)
+  case $compileMode in
+    st_component_mode)
+    chmod +x ./build_st_component.sh
+    ./build_st_component.sh
+    ;;
+    *)
+    echo "No compile mode match found"
+    ;;
+  esac
 else
   chmod +x ./build.sh
-  ./build.sh $1
+  ./build.sh "$1"
 fi
