50773fbc1e7af3f1954453469f20e82e2899d5ca
lwwilkov <lwwilkov@amazon.com>
2021-01-11 10:44:38 -0800

    Toolchain and CI for mips32 & aarch64

--- .github/build.sh
+++ .github/build.sh
@@ -10,8 +10,14 @@ if [ "$compileModeArgument" = "compile-mode" ]; then
     st_component_mode)
     compileMode="st_component_mode"
     ;;
-    rpi_cross_mode)
-    compileMode="rpi_cross_mode"
+    armhf_cross_mode)
+    compileMode="armhf_cross_mode"
+    ;;
+    mips_cross_mode)
+    compileMode="mips_cross_mode"
+    ;;
+    aarch64_cross_mode)
+    compileMode="aarch64_cross_mode"
     ;;
     *)
     echo "No compile mode match found"
@@ -73,28 +79,76 @@ case $compileMode in
     echo "Building in ST component mode"
     cmake ../ -DBUILD_SDK=OFF -DBUILD_TEST_DEPS=OFF -DEXCLUDE_JOBS=ON -DEXCLUDE_DD=ON -DEXCLUDE_FP=ON -DDISABLE_MQTT=ON
     ;;
-    rpi_cross_mode)
+    armhf_cross_mode)
     apt-get update
     apt-get install --assume-yes software-properties-common
     apt-get install --assume-yes build-essential
     apt-get install --assume-yes g++-arm-linux-gnueabihf
+    apt-get install --assume-yes gcc-arm-linux-gnueabihf
     apt-get install --assume-yes gdb-multiarch
-    git clone https://github.com/raspberrypi/tools.git --depth=1 pitools
-    cpwd="$PWD"
-    export CROSSCOMP_DIR=$cpwd/pitools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin
     wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
     tar -xvzf openssl-1.1.1.tar.gz
     export INSTALL_DIR=/usr/lib/arm-linux-gnueabihf
     cd openssl-1.1.1
     ./Configure linux-generic32 shared \
-    --prefix=$INSTALL_DIR --openssldir=$INSTALL_DIR/openssl \
-    --cross-compile-prefix=$CROSSCOMP_DIR/arm-linux-gnueabihf-
+      --prefix=$INSTALL_DIR --openssldir=$INSTALL_DIR/openssl \
+      --cross-compile-prefix=usr/bion/arm-linux-gnueabihf-
     make depend
-    make
+    make -j 4
     make install
     cd ..
-    cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-rpi3-armhf.cmake ../ || true
-    cmake -DCMAKE_TOOLCHAIN_FILE=../Toolchain-rpi3-armhf.cmake ../
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-armhf.cmake ../ || true
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-armhf.cmake ../
+    cmake --build . --target aws-iot-device-client
+    cmake --build . --target test-aws-iot-device-client
+    exit 0
+    ;;
+    #################################
+    mips_cross_mode)
+    apt-get update
+    apt-get install --assume-yes software-properties-common
+    apt-get install --assume-yes build-essential
+    apt-get install --assume-yes g++-mips-linux-gnu
+    apt-get install --assume-yes gcc-mips-linux-gnu
+    apt-get install --assume-yes gdb-multiarch
+    wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+    tar -xvzf openssl-1.1.1.tar.gz
+    export INSTALL_DIR=/usr/lib/mips-linux-gnu
+    cd openssl-1.1.1
+    ./Configure linux-mips32 shared \
+      --prefix=$INSTALL_DIR --openssldir=$INSTALL_DIR/openssl \
+      --cross-compile-prefix=/usr/bin/mips-linux-gnu-
+    make depend
+    make -j 4
+    make install
+    cd ..
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../ || true
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../
+    cmake --build . --target aws-iot-device-client
+    cmake --build . --target test-aws-iot-device-client
+    exit 0
+    ;;
+    #################################
+    aarch64_cross_mode)
+    apt-get update
+    apt-get install --assume-yes software-properties-common
+    apt-get install --assume-yes build-essential
+    apt-get install --assume-yes g++-aarch64-linux-gnu
+    apt-get install --assume-yes gcc-aarch64-linux-gnu
+    apt-get install --assume-yes gdb-multiarch
+    wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+    tar -xvzf openssl-1.1.1.tar.gz
+    export INSTALL_DIR=/usr/lib/aarch64-linux-gnu
+    cd openssl-1.1.1
+    ./Configure linux-aarch64 shared \
+      --prefix=$INSTALL_DIR --openssldir=$INSTALL_DIR/openssl \
+      --cross-compile-prefix=/usr/bin/aarch64-linux-gnu-
+    make depend
+    make -j 4
+    make install
+    cd ..
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-aarch64.cmake ../ || true
+    cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-aarch64.cmake ../
     cmake --build . --target aws-iot-device-client
     cmake --build . --target test-aws-iot-device-client
     exit 0
