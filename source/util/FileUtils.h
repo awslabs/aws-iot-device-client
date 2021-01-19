a90e95657889b1ad0ea311e06ed730e720b04926
lwwilkov <lwwilkov@amazon.com>
2021-01-19 06:37:22 -0800

    Add missing headers

--- source/util/FileUtils.h
+++ source/util/FileUtils.h
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef AWS_IOT_DEVICE_CLIENT_FILEUTILS_H
 #define AWS_IOT_DEVICE_CLIENT_FILEUTILS_H
 
