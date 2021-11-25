fa76a665f0d033a070275d057f400267125fd6a1
Harsh Gandhi <harsgand@amazon.com>
2021-11-24 16:00:27 -0800

    Clang Formatting

--- source/config/Config.cpp
+++ source/config/Config.cpp
@@ -615,7 +615,8 @@ bool PlainConfig::Jobs::LoadFromJson(const Crt::JsonView &json)
     }
 
     jsonKey = JSON_KEY_HANDLER_DIR;
-    if (json.ValueExists(jsonKey)) {
+    if (json.ValueExists(jsonKey))
+    {
         handlerDir = FileUtils::ExtractExpandedPath(json.GetString(jsonKey).c_str());
     }
 
