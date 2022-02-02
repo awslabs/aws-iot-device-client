302b45aa1854dc350eabbc1478edb0ca33556c84
Tim Mattison <tim@mattison.org>
2022-02-02 13:13:18 -0500

    Switch to fork from vfork
    Avoids deprecation warnings and works on MacOS

--- source/jobs/JobEngine.cpp
+++ source/jobs/JobEngine.cpp
@@ -270,7 +270,7 @@ int JobEngine::exec_cmd(string operation, PlainJobDocument::JobAction action)
 
     int execResult;
     int returnCode;
-    int pid = vfork();
+    int pid = fork();
     if (pid < 0)
     {
         LOGM_ERROR(TAG, "Failed to create child process, fork returned %d", pid);
