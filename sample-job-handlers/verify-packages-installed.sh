d84655a95b017e92f1537de81195efc75d1bd68a
Shane Gable <shangabl@amazon.com>
2022-03-15 17:23:34 -0700

    Adds additional check for package
    The handler was failing in the event that both dpkg and rpm were both installed

--- sample-job-handlers/verify-packages-installed.sh
+++ sample-job-handlers/verify-packages-installed.sh
@@ -8,28 +8,29 @@ packages=$@
 echo "Username: $user"
 echo "Packages to verify: $packages"
 
-if command -v "rpm" > /dev/null; then
+if id "$user" 2>/dev/null && command -v "sudo" > /dev/null; then
   for package in $packages
   do
-    if id "$user" 2>/dev/null && command -v "sudo" > /dev/null; then
-      sudo -u "$user" -n rpm -q "$package"
-    else
-      echo "username or sudo command not found"
-      rpm -q "$package"
+    if command -v "rpm" >/dev/null; then
+      if command sudo -u "$user" -n rpm -q "$package" > /dev/null; then
+        continue
+      fi
     fi
-  done
-elif command -v "dpkg" > /dev/null; then
-  # Using -s flag instead of -l for dpkg based on https://github.com/bitrise-io/bitrise/issues/433
-  for package in $packages
-  do
-    if id "$user" 2>/dev/null && command -v "sudo" > /dev/null; then
+    if command -v "dpkg" >/dev/null; then
       sudo -u "$user" -n dpkg -s "$package"
-    else
-      echo "username or sudo command not found"
-      dpkg -s "$package"
     fi
   done
 else
-  >&2 echo "No suitable package manager found"
-  exit 1
+  echo "username or sudo command not found"
+  for package in $packages
+  do
+    if command -v "rpm" >/dev/null; then
+      if command rpm -q "$package" > /dev/null; then
+        continue
+      fi
+    fi
+    if command -v "dpkg" >/dev/null; then
+      dpkg -s "$package"
+    fi
+  done
 fi;
