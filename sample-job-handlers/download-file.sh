aa2b3f738431fa7e8b0f374b5f8eae4dc5b0e64f
Shane Gable <shangabl@amazon.com>
2022-03-22 15:06:16 -0700

    Modifies download file handler to allow for passing in a directory to store file

--- sample-job-handlers/download-file.sh
+++ sample-job-handlers/download-file.sh
@@ -12,10 +12,18 @@ if command -v "wget" > /dev/null
 then
     echo "Using wget for downloading user specified file"
     if id "$user" 2>/dev/null && command -v "sudo" > /dev/null; then
-      sudo -u "$user" -n wget --quiet -O "$outputFile" "$fileUrl"
+      if [ -d "$outputFile" ]; then
+        sudo -u "$user" -n wget --quiet -P "$outputFile" "$fileUrl"
+      else
+        sudo -u "$user" -n wget --quiet -O "$outputFile" "$fileUrl"
+      fi
     else
       echo "username or sudo command not found"
-      wget --quiet -O "$outputFile" "$fileUrl"
+      if [ -d "$outputFile" ]; then
+        wget --quiet -P "$outputFile" "$fileUrl"
+      else
+        wget --quiet -O "$outputFile" "$fileUrl"
+      fi
     fi
 else
     >&2 echo "Wget software package not installed on the device. Use the \"install-packages.sh\" operation to install the wget software package on this device."
