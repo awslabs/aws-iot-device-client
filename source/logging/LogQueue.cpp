53cf23acd2f3519bb3a2c8581692743a193af3a3
Roger Zhong <rgz@amazon.com>
2022-04-15 16:46:38 -0700

    testing WITH change

--- source/logging/LogQueue.cpp
+++ source/logging/LogQueue.cpp
@@ -25,8 +25,7 @@ std::unique_ptr<LogMessage> LogQueue::getNextLog()
     unique_lock<mutex> readLock(queueLock);
     while (logQueue.empty() && !isShutdown)
     {
-        newLogNotifier.wait(readLock);
-        // newLogNotifier.wait_for(readLock, chrono::milliseconds (100));
+        newLogNotifier.wait_for(readLock, chrono::milliseconds (100));
     }
     if (logQueue.empty())
     {
