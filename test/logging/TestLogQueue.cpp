b9d65b3505facea0b1ebe38c887c94af17ac6962
Roger Zhong <rgz@amazon.com>
2022-04-18 17:06:36 -0700

    test performance impact

--- test/logging/TestLogQueue.cpp
+++ test/logging/TestLogQueue.cpp
@@ -85,8 +85,8 @@ TEST_F(LogQueueTest, notifyAllOnShutdown)
 
     logQueue->shutdown();
 
-    cv.wait_for(lock1, chrono::milliseconds(200));
-    cv2.wait_for(lock2, chrono::milliseconds(200));
+    cv.wait_for(lock1, chrono::milliseconds(400));
+    cv2.wait_for(lock2, chrono::milliseconds(400));
 
     ASSERT_TRUE(processed && processed2);
 }
