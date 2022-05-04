898f0d9ca5062c97e47b1828cb430a270d3abe04
Shane Gable <shangabl@amazon.com>
2022-05-04 15:17:56 -0500

    Fix for invalid root-ca file paths

--- source/config/Config.cpp
+++ source/config/Config.cpp
@@ -113,7 +113,16 @@ bool PlainConfig::LoadFromJson(const Crt::JsonView &json)
     {
         if (!json.GetString(jsonKey).empty())
         {
-            rootCa = FileUtils::ExtractExpandedPath(json.GetString(jsonKey).c_str());
+            basic_string<char> path = FileUtils::ExtractExpandedPath(json.GetString(jsonKey).c_str());
+            if (!FileUtils::FileExists(path))
+            {
+                LOGM_ERROR(Config::TAG, "Path %s to RootCA is invalid. Ignoring... Will attempt to use default trust store.", path.c_str());
+                rootCa = "";
+            }
+            else
+            {
+                rootCa = FileUtils::ExtractExpandedPath(json.GetString(jsonKey).c_str());
+            }
         }
         else
         {
