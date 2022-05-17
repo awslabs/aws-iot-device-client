6b1232efa01d013e698370a7728d15b3926a33b3
Roger Zhong <rgz@amazon.com>
2022-05-17 14:52:12 -0700

    remove cmake cache

--- .github/build.sh
+++ .github/build.sh
@@ -164,11 +164,13 @@ case $compileMode in
     # Fix for the Cmake executing build of the sdk which errors out linking incorrectly to openssl
     if [ "$sharedLibs" = true ]; then
       cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../ || true
+      rm -rf CMakeCache.txt CMakeFiles/
       cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../
       make install DESTDIR=./shared_install_dir
       chmod 0777 ./shared_install_dir
     else
       cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../ || true
+      rm -rf CMakeCache.txt CMakeFiles/
       cmake -DCMAKE_TOOLCHAIN_FILE=../cmake-toolchain/Toolchain-mips.cmake ../
     fi
     cmake --build . --target aws-iot-device-client
