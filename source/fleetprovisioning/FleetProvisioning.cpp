7557ea96c8f5bc5a350d9f0c8f4bc8cbf6876807
Harsh Gandhi <63674702+HarshGandhi-AWS@users.noreply.github.com>
2022-07-29 16:57:47 -0700

    Removed code which was redundantly creating  dir with incorrect permissions.  (#305)
    * Removed code which was redundantly creating  dir with incorrect permission.
    
    * resolved indentation errors
    
    * resolved indentation errors
    
    * updated unit tests
    
    * updated log statements

--- source/fleetprovisioning/FleetProvisioning.cpp
+++ source/fleetprovisioning/FleetProvisioning.cpp
@@ -494,7 +494,8 @@ bool FleetProvisioning::ProvisionDevice(shared_ptr<SharedCrtResourceManager> fpC
         LOG_INFO(TAG, "Fleet Provisioning Feature has been started.");
 
         bool didSetup = FileUtils::CreateDirectoryWithPermissions(keyDir.c_str(), S_IRWXU) &&
-                        FileUtils::CreateDirectoryWithPermissions(Config::DEFAULT_CONFIG_DIR, S_IRWXU);
+                        FileUtils::CreateDirectoryWithPermissions(
+                            Config::DEFAULT_CONFIG_DIR, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IROTH | S_IXOTH);
         if (!didSetup)
         {
             LOGM_ERROR(
