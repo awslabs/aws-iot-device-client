e31406f96778dc98a257fba76d591261c706b765
Shane Gable <shangabl@amazon.com>
2022-08-17 13:35:13 -0500

    fix

--- source/devicedefender/ReportTaskWrapper.cpp
+++ source/devicedefender/ReportTaskWrapper.cpp
@@ -3,17 +3,20 @@
 
 #include "ReportTaskWrapper.h"
 
-Iot::DeviceClient::DeviceDefender::ReportTaskWrapper::ReportTaskWrapper(
+using namespace Aws::Iot;
+using namespace std;
+
+DeviceClient::DeviceDefender::ReportTaskWrapper::ReportTaskWrapper(
     shared_ptr<Iotdevicedefenderv1::ReportTask> task)
     : task(task)
 {
 }
 
-int Iot::DeviceClient::DeviceDefender::ReportTaskWrapper::StartTask()
+int DeviceClient::DeviceDefender::ReportTaskWrapper::StartTask()
 {
     return task->StartTask();
 }
-void Iot::DeviceClient::DeviceDefender::ReportTaskWrapper::StopTask()
+void DeviceClient::DeviceDefender::ReportTaskWrapper::StopTask()
 {
     task->StopTask();
 }
