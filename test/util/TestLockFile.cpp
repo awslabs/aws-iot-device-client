0be0ce55f948e29889b8e3d71f778a9d6c0205c3
Roger Zhong <rgz@amazon.com>
2022-10-10 10:23:28 -0700

    fix tests

--- test/util/TestLockFile.cpp
+++ test/util/TestLockFile.cpp
@@ -14,16 +14,18 @@ using namespace Aws::Iot::DeviceClient::Util;
 TEST(LockFile, normalCreation)
 {
     string path = "/run/lock/";
-    string filename = "devicecl.lock";
-    string thingname = "thing";
-    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "./aws-iot-device-client", thingname});
+    string fileName = "devicecl.lock";
+    string thingName = "thing";
+    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "./aws-iot-device-client", thingName});
 
-    ifstream fileIn(path + filename);
+    ifstream fileIn(path + fileName);
     ASSERT_TRUE(fileIn);
 
     string storedPid;
-    if (fileIn >> storedPid)
+    string storedName;
+    if (fileIn >> storedName && fileIn >> storedPid)
     {
+        ASSERT_STREQ(thingName.c_str(), storedName.c_str());
         ASSERT_STREQ(to_string(getpid()).c_str(), storedPid.c_str());
     }
 }
@@ -31,57 +33,59 @@ TEST(LockFile, normalCreation)
 TEST(LockFile, earlyDeletion)
 {
     string path = "/run/lock/";
-    string filename = "devicecl.lock";
-    string thingname = "thing";
-    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingname});
+    string fileName = "devicecl.lock";
+    string thingName = "thing";
+    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingName});
     lockFile.reset();
 
-    ifstream fileIn(path + filename);
+    ifstream fileIn(path + fileName);
     ASSERT_FALSE(fileIn);
 }
 
 TEST(LockFile, multipleFiles)
 {
     string path = "/run/lock/";
-    string thingname = "thing";
-    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingname});
+    string thingName = "thing";
+    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingName});
 
-    EXPECT_THROW(unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingname}), std::runtime_error);
+    EXPECT_THROW(unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingName}), std::runtime_error);
 }
 
 TEST(LockFile, multipleFilesWithExtendedPath)
 {
     string path = "/run/lock/";
-    string thingname = "thing";
-    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingname});
+    string thingName = "thing";
+    unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingName});
 
-    EXPECT_THROW(unique_ptr<LockFile>(new LockFile{path, "directory/test-aws-iot-device-client", thingname}), std::runtime_error);
+    EXPECT_THROW(unique_ptr<LockFile>(new LockFile{path, "directory/test-aws-iot-device-client", thingName}), std::runtime_error);
 }
 
 TEST(LockFile, staleFile)
 {
     string path = "/run/lock/";
-    string filename = "devicecl.lock";
-    string thingname = "thing";
+    string fileName = "devicecl.lock";
+    string thingName = "thing";
     string pidMax;
     ifstream pidFile("/proc/sys/kernel/pid_max");
     if (pidFile && pidFile >> pidMax)
     {
-        ofstream fileOut(path + filename);
+        ofstream fileOut(path + fileName);
         if (fileOut)
         {
             fileOut << pidMax;
         }
         fileOut.close();
 
-        unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingname});
+        unique_ptr<LockFile> lockFile = unique_ptr<LockFile>(new LockFile{path, "test-aws-iot-device-client", thingName});
 
-        ifstream fileIn(path + filename);
+        ifstream fileIn(path + fileName);
         ASSERT_TRUE(fileIn);
 
         string storedPid;
-        if (fileIn >> storedPid)
+        string storedName;
+        if (fileIn >> storedName && fileIn >> storedPid)
         {
+            ASSERT_STREQ(thingName.c_str(), storedName.c_str());
             ASSERT_STREQ(to_string(getpid()).c_str(), storedPid.c_str());
         }
     }
