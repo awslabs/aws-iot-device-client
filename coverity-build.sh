2f083d3763cc04ea548d8019d47f602d21ed1bad
Roger Zhong <rgz@amazon.com>
2022-12-20 14:04:02 -0800

    add cert store trigger for main branch only

--- coverity-build.sh
+++ coverity-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-wget https://scan.coverity.com/download/linux64 --post-data "token=$COVERITY_TOKEN&project=aws-iot-device-client" -O coverity_tool.tgz \
+wget --ca-certificate=/etc/ssl/certs/ca-certificates.crt https://scan.coverity.com/download/linux64 --post-data "token=$COVERITY_TOKEN&project=aws-iot-device-client" -O coverity_tool.tgz \
 && tar zxvf coverity_tool.tgz --one-top-level=cov --strip-components=1
 
-export PATH=$PATH:/tmp/cov/bin/
+export PATH=$PATH:$(pwd)/cov/bin/
 
 rm -rf build
 mkdir build
@@ -12,11 +12,10 @@ cmake ..
 cov-build --dir cov-int cmake --build . --target aws-iot-device-client
 chmod +x aws-iot-device-client
 tar czvf aws-iot-device-client.tgz cov-int
-ls
 
 curl --form token="$COVERITY_TOKEN" \
   --form email="$COVERITY_EMAIL" \
   --form file=@aws-iot-device-client.tgz \
-  --form version="Version" \
-  --form description="Description" \
+  --form version="$(./aws-iot-device-client --version)" \
+  --form description="aws-iot-device-client" \
   https://scan.coverity.com/builds?project=aws-iot-device-client
