53f196cd1a52f737be7b44617272ab9dd82c4267
Roger Zhong <rgz@amazon.com>
2022-12-20 17:01:34 -0800

    remove ls command

--- static-checks/report-coverage.sh
+++ static-checks/report-coverage.sh
@@ -1,7 +1,6 @@
 # Compile and run the tests before running this script.
 
-cd ../build/test/CMakeFiles/test-aws-iot-device-client.dir/__/source
-lcov --capture --directory . --output-file coverage.info
+lcov --capture --directory ../build/test/CMakeFiles/test-aws-iot-device-client.dir/__/source --output-file coverage.info
 genhtml coverage.info --output-directory out
 
 # To view the results, open the generated index.html file in a web browser.
