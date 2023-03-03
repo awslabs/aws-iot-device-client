09407383aa26169574bdc5099e5e0d5306c26b23
Roger Zhong <rgz@amazon.com>
2023-03-03 10:10:31 -0800

    remove all thrown exceptions in lockfile handler

--- source/util/LockFile.cpp
+++ source/util/LockFile.cpp
@@ -34,22 +34,18 @@ LockFile::LockFile(const std::string &filedir, const std::string &process, const
             ifstream cmd(processPath.c_str());
             if (cmd && cmd >> cmdline && cmdline.find(basename) != string::npos)
             {
-                LOGM_ERROR(
+                LOGM_WARN(
                     TAG,
                     "Pid %s associated with active process %s in lockfile: %s",
                     Sanitize(storedPid).c_str(),
                     Sanitize(process).c_str(),
                     Sanitize(fullPath).c_str());
-
-                throw runtime_error{"Device Client is already running."};
             }
         }
         // remove stale pid file
         if (remove(fullPath.c_str()))
         {
-            LOGM_ERROR(TAG, "Unable to remove stale lockfile: %s", Sanitize(fullPath).c_str());
-
-            throw runtime_error{"Error removing stale lockfile."};
+            LOGM_WARN(TAG, "Unable to remove stale lockfile: %s", Sanitize(fullPath).c_str());
         }
     }
     fileIn.close();
@@ -57,7 +53,7 @@ LockFile::LockFile(const std::string &filedir, const std::string &process, const
     FILE *file = fopen(fullPath.c_str(), "wx");
     if (!file)
     {
-        LOGM_ERROR(
+        LOGM_WARN(
             TAG, "Unable to open lockfile. File may be in use or does not exist: %s", Sanitize(fullPath).c_str());
     }
     else
