2c1af4ec3e7c4028fb1d6a63a77459aa26c9244a
Miguel Cano <macano953@users.noreply.github.com>
2023-07-04 13:15:10 +0200

    Fix docker-build.sh
    There is an additional level of indirection required for the script to be able to build the container.

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,4 @@
 # Defaults to ubuntu (18.04). Other options are amazonlinux and ubi8
 os=${1:-"ubuntu"}
 
-docker build . --file .github/docker-images/base-images/"$os"/Dockerfile
+docker build . --file .github/docker-images/base-images/device-client/"$os"/Dockerfile
