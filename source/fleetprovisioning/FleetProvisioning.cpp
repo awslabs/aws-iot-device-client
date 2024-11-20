0e3ee43d3ac7cac8f1ff4ef84d42f300c7ef8236
tony-josi-aws <tonyjosi@amazon.com>
2024-11-20 13:34:26 +0530

    Fix formatting wrt clanng format

--- source/fleetprovisioning/FleetProvisioning.cpp
+++ source/fleetprovisioning/FleetProvisioning.cpp
@@ -120,8 +120,7 @@ bool FleetProvisioning::WriteCSRCertToDirectory(CreateCertificateFromCsrResponse
 bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient identityClient)
 {
     LOG_INFO(TAG, "Provisioning new device certificate and private key using CreateKeysAndCertificate API");
-    auto onKeysAcceptedSubAck = [this](int ioErr)
-    {
+    auto onKeysAcceptedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -133,8 +132,7 @@ bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient i
         keysAcceptedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onKeysRejectedSubAck = [this](int ioErr)
-    {
+    auto onKeysRejectedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -146,8 +144,7 @@ bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient i
         keysRejectedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onKeysPublishSubAck = [this](int ioErr)
-    {
+    auto onKeysPublishSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -159,8 +156,7 @@ bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient i
         keysPublishCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onKeysAccepted = [this](CreateKeysAndCertificateResponse *response, int ioErr)
-    {
+    auto onKeysAccepted = [this](CreateKeysAndCertificateResponse *response, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_INFO(TAG, "CreateKeysAndCertificateResponse certificateId: %s.", response->CertificateId->c_str());
@@ -181,8 +177,7 @@ bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient i
         }
     };
 
-    auto onKeysRejected = [this](ErrorResponse *error, int ioErr)
-    {
+    auto onKeysRejected = [this](ErrorResponse *error, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -257,8 +252,7 @@ bool FleetProvisioning::CreateCertificateAndKey(Iotidentity::IotIdentityClient i
 bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient identityClient)
 {
     LOG_INFO(TAG, "Provisioning new device certificate using CreateCertificateFromCsr API");
-    auto onCsrAcceptedSubAck = [this](int ioErr)
-    {
+    auto onCsrAcceptedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -270,8 +264,7 @@ bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient
         csrAcceptedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onCsrRejectedSubAck = [this](int ioErr)
-    {
+    auto onCsrRejectedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -283,8 +276,7 @@ bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient
         csrRejectedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onCsrPublishSubAck = [this](int ioErr)
-    {
+    auto onCsrPublishSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -296,8 +288,7 @@ bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient
         csrPublishCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onCsrAccepted = [this](CreateCertificateFromCsrResponse *response, int ioErr)
-    {
+    auto onCsrAccepted = [this](CreateCertificateFromCsrResponse *response, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_INFO(TAG, "CreateCertificateFromCsrResponse certificateId: %s. ***", response->CertificateId->c_str());
@@ -318,8 +309,7 @@ bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient
         }
     };
 
-    auto onCsrRejected = [this](ErrorResponse *error, int ioErr)
-    {
+    auto onCsrRejected = [this](ErrorResponse *error, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -397,8 +387,7 @@ bool FleetProvisioning::CreateCertificateUsingCSR(Iotidentity::IotIdentityClient
 }
 bool FleetProvisioning::RegisterThing(Iotidentity::IotIdentityClient identityClient)
 {
-    auto onRegisterAcceptedSubAck = [this](int ioErr)
-    {
+    auto onRegisterAcceptedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -410,8 +399,7 @@ bool FleetProvisioning::RegisterThing(Iotidentity::IotIdentityClient identityCli
         registerAcceptedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onRegisterRejectedSubAck = [this](int ioErr)
-    {
+    auto onRegisterRejectedSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -423,8 +411,7 @@ bool FleetProvisioning::RegisterThing(Iotidentity::IotIdentityClient identityCli
         registerRejectedCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onRegisterPublishSubAck = [this](int ioErr)
-    {
+    auto onRegisterPublishSubAck = [this](int ioErr) {
         if (ioErr != AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
@@ -436,8 +423,7 @@ bool FleetProvisioning::RegisterThing(Iotidentity::IotIdentityClient identityCli
         registerPublishCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onRegisterAccepted = [this](RegisterThingResponse *response, int ioErr)
-    {
+    auto onRegisterAccepted = [this](RegisterThingResponse *response, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_INFO(TAG, "RegisterThingResponse ThingName: %s.", response->ThingName->c_str());
@@ -452,8 +438,7 @@ bool FleetProvisioning::RegisterThing(Iotidentity::IotIdentityClient identityCli
         registerThingCompletedPromise.set_value(ioErr == AWS_OP_SUCCESS);
     };
 
-    auto onRegisterRejected = [this](ErrorResponse *error, int ioErr)
-    {
+    auto onRegisterRejected = [this](ErrorResponse *error, int ioErr) {
         if (ioErr == AWS_OP_SUCCESS)
         {
             LOGM_ERROR(
